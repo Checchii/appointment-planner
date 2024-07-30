49f9923e76f2543699136eada1cf2ee6054bfd88
Sebastian Checchi <sebastian.checchi@gmail.com>
2024-07-30 11:49:02 -0400

    added AppointmentsPage component

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,53 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({ appointments, contacts, createNewAppointment }) => {
+  const [name, setName] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+
+    const isDuplicate = appointments.some(appointment => 
+      appointment.name === name && 
+      appointment.date === date &&
+      appointment.time === time &&
+      appointment.contact === contact);
+    
+    if (isDuplicate) {
+      alert("An appointment with these details already exists.");
+    } else {
+      createNewAppointment(name, contact, date, time);
+      setName('');
+      setContact('');
+      setDate('');
+      setTime('');
+    }
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+        name={name}
+        onNameChange={setName}
+        contact={contact}
+        onContactChange={setContact}
+        contacts={contacts}
+        date={date}
+        onDateChange={setDate}
+        time={time}
+        onTimeChange={setTime}
+        handleSubmit={handleSubmit}
+        /> 
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList  tiles={appointments} />
       </section>
     </div>
   );
