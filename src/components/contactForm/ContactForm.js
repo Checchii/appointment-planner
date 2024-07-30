9e31d50fc3eaeac9868d22f29c401b4153b28d26
Sebastian Checchi <sebastian.checchi@gmail.com>
2024-07-29 22:55:30 -0400

    added contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,15 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+      <label htmlFor="phone">Phone:</label>
+      <input type="tel" value={phone} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" required onChange={(e) => setPhone(e.target.value)}></input>
+      <label htmlFor="email">Email:</label>
+      <input type="email" value={emai} onChange={(e) => setEmail(e.target.value)}></input>
+      <button type="submit">Add Contact</button>
+    </form>
   );
 };
 
