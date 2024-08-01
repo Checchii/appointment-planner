392950d7734af52c984bfe4d09e2b668b936e8d9
Sebastian Checchi <sebastian.checchi@gmail.com>
2024-07-31 20:27:37 -0400

    implemented contactPicker component

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ( { contacts = [], value, name, onChange}) => {
   return (
-    <></>
+    <select onChange={onChange} value={value} name={name}>
+      <option value="">No Contact Selected</option>
+      {contacts.map((contact, index) => (
+        <option key={index} value={contact.name}>{contact.name}</option>
+      ))}
+    </select>
   );
 };
